1cf95cf72730fecd222c89e41bbbd332ee3ae67a
Gonçalo Silva <gnorsilva@gmail.com>
2013-01-15 09:00:43 +0000

    working on exercises 1.6 & 1.7

--- exercises/sicp/chapter1/1_1_elements_of_programming.clj
+++ exercises/sicp/chapter1/1_1_elements_of_programming.clj
@@ -35,13 +35,13 @@
         5 + 4 + (2 - (3 - (6 + 4/5))) / 3(6 - 2)(2 - 7)"
   (/
     (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
-    (* 3 (- 6 2)(- 2 7))
+    (* 3 (- 6 2) (- 2 7))
     ) => (/ (- (/ 74 5)) 60))
 
 
-(fact "1.3 define a procedure that takes three numbers and returns the sum of the squares of the two largest numbers"
+(fact "1.3 - define a procedure that takes three numbers and returns the sum of the squares of the two largest numbers"
 
-  (defn square [n] (* n n))             *
+  (defn square [n] (* n n))
 
   (defn sum-of-squares [x y] (+ (square x) (square y)))
 
@@ -76,4 +76,73 @@
   ; for normal-order evaluation p is never evaluated and so the program can
   ; evaluate the correct result
 
-  (eval-test 0 p) => 0 )
+  (eval-test 0 p) => 0)
+
+
+
+; *** Difference between function (math) and procedure (CS) is describing properties of things
+; and describing how to do things, i.e. declarative vs imperative knowledge ***
+
+
+
+(fact "1.6 - What happens when Allysa P. Hacker tries to use a new-if to compute square roots?"
+
+  (defn average [x y]
+    (double (/ (+ x y) 2)))
+  ; casting these to double to loose clojure's inbuilt rational number fractions
+
+  (defn improve [guess x]
+    (average guess (/ x guess)))
+
+  (defn abs [x]
+    (if (< x 0) (- x) x))
+
+  (defn good-enough? [guess x]
+    (< (abs (- (square guess) x)) 0.0001))
+
+  (defn sqrt-iter [guess x]
+    (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+  (defn sqrt [x]
+    (sqrt-iter 1 x))
+
+  (sqrt 2) => 1.4142156862745097
+  (sqrt 4) => 2.0000000929222947
+
+
+  (defn new-if [predicate then-clause else-clause]
+    (cond predicate then-clause
+      :else else-clause))
+
+  ; The new-if is of applicative-order evaluation and so the procedure never terminates
+  ; as both clauses will be evaluated before the 'predicate' test!
+  )
+
+(fact "1.7"
+  (fact "explain why the above 'good-enough?' function is not effective for small and large numbers"
+
+    (sqrt 0.000000001) => 0.007812542664015912 ; WRONG! should have been 0.00003162277
+    ; (sqrt 10000000000000000000000000000000) => STACK OVERFLOW!
+
+    ; For small numbers 0.0001 is not an accurate threshold
+    ; For large numbers it would take a long time to reach a precision of 0.0001
+
+    )
+
+  (pending-fact "implement an alternative 'good-enough?' which uses fractions to decide when to stop"
+
+    (defn round-it [x]
+      (Double/valueOf (format "%.4f" (double x))))
+
+    (defn guess-ratio [guess x]
+      (abs (/ (square guess) x)))
+
+    (defn good-enough? [guess x]
+      (and (< (guess-ratio guess x) 1) (> (guess-ratio guess x) 0.5)))
+
+    (sqrt 2) => 1.4142156862745097
+    (sqrt 4) => 2.0000000929222947
+    (sqrt 0.000000001) => 0.00003162277)
+  )
